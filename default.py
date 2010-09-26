172e4cfb3cf0ef242b09d86265aa9e9d70ccbc1c
Zsolt Török <zsolt@zsolttorok.com>
2010-09-26 16:21:38 +0200

    Initial commit.

---
+++ default.py
@@ -0,0 +1,76 @@
+import sys
+import xbmc, xbmcgui, xbmcplugin
+import urllib
+
+# plugin modes
+MODE_FIRST = 10
+MODE_SECOND = 20
+
+# parameter keys
+PARAMETER_KEY_MODE = "mode"
+PARAMETER_KEY_NAME = "name"
+
+# menu item names
+FIRST_SUBMENU = "First Submenu"
+SECOND_SUBMENU = "Second Submenu"
+
+# plugin handle
+handle = int(sys.argv[1])
+
+# utility functions
+def parameters_string_to_dict(parameters):
+    ''' Convert parameters encoded in a URL to a dict. '''
+    paramDict = {}
+    if parameters:
+        paramPairs = parameters[1:].split("&")
+        for paramsPair in paramPairs:
+            paramSplits = paramsPair.split('=')
+            if (len(paramSplits)) == 2:
+                paramDict[paramSplits[0]] = paramSplits[1]
+    return paramDict
+
+def addDirectoryItem(name, isFolder=True, parameters={}):
+    ''' Add a list item to the XBMC UI.'''
+    li = xbmcgui.ListItem(name)
+    url = sys.argv[0] + '?' + urllib.urlencode(parameters)
+    return xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=url, listitem=li, isFolder=isFolder)
+
+
+# UI builder functions
+def show_root_menu():
+    ''' Show the plugin root menu. '''
+    addDirectoryItem(name=FIRST_SUBMENU, parameters={ PARAMETER_KEY_MODE: MODE_FIRST, PARAMETER_KEY_NAME: FIRST_SUBMENU }, isFolder=True)
+    addDirectoryItem(name=SECOND_SUBMENU, parameters={ PARAMETER_KEY_MODE: MODE_SECOND, PARAMETER_KEY_NAME: SECOND_SUBMENU }, isFolder=True)
+    xbmcplugin.endOfDirectory(handle=handle, succeeded=True)
+    
+def show_first_submenu():
+    ''' Show first submenu. '''
+    for i in range(0, 5):
+        name = "%s Item %d" % (FIRST_SUBMENU, i)
+        addDirectoryItem(name, isFolder=False)
+    xbmcplugin.endOfDirectory(handle=handle, succeeded=True)
+
+def show_second_submenu():
+    ''' Show second submenu. '''
+    for i in range(0, 10):
+        name = "%s Item %d" % (SECOND_SUBMENU, i)
+        addDirectoryItem(name, isFolder=False)
+    xbmcplugin.endOfDirectory(handle=handle, succeeded=True)
+
+# parameter values
+params = parameters_string_to_dict(sys.argv[2])
+name = urllib.unquote_plus(params.get(PARAMETER_KEY_NAME, ""))
+mode = int(params.get(PARAMETER_KEY_MODE, "0"))
+print "##########################################################"
+print("Name: %s" % name)
+print("Mode: %s" % mode)
+print "##########################################################"
+
+# Depending on the mode, call the appropriate function to build the UI.
+if not sys.argv[2]:
+    # new start
+    ok = show_root_menu()
+elif mode == MODE_FIRST:
+    ok = show_first_submenu()
+elif mode == MODE_SECOND:
+    ok = show_second_submenu()
